a9393cb916c3b8ee2692e43d4dafe40913419f72
Razmik Melikbekyan <melikbekyan@yahoo.com>
2020-03-04 00:38:49 +0100

    FIX: fixing training hanging (#345)

--- autogluon/utils/__init__.py
+++ autogluon/utils/__init__.py
@@ -9,3 +9,4 @@ from .sync_remote import *
 from .custom_queue import Queue
 from .defaultdict import keydefaultdict
 from .util_decorator import classproperty
+from .custom_process import CustomProcess
