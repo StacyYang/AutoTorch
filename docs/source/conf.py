2fe3a8a059e2e1034fb2c415054300b501a27a00
Stacy Yang <yangxiaoqian0828@gmail.com>
2020-04-12 15:08:00 -0700

    add colab (#13)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,7 +139,9 @@ html_theme_path = [autorch_sphinx_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
+
 html_theme_options = {
+    'pytorch_project': 'tutorials',
     'collapse_navigation': False,
     'display_version': True,
     'logo_only': True,
